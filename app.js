02586fa0c31c930544033eda4dffab501dc5d197
Clara Lisboa <mclisboa2006@gmail.com>
2025-03-17 23:49:01 -0300

    amigo secreto

---
+++ app.js
@@ -0,0 +1,20 @@
+let amigos = [];
+
+function exibirTextoNaTela(tag, texto) {
+    let campo = document.querySelector(tag);
+    campo.innerHTML = texto;
+}
+
+function adicionarAmigo(){
+    let nome = document.querySelector('input').value;
+    if(nome.trim() == ''){
+        exibirTextoNaTela('h2', 'Por favor insira um nome.');
+        return;
+    } else {
+    amigos.push(nome);
+    console.log(amigos);
+    }
+    exibirTextoNaTela('h2', 'Digite o nome dos seus amigos');
+    nome = document.querySelector('input');
+    nome.value = '';
+}
