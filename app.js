0e2927cd1126cf9857306ef590e14a45d3e2f79b
Clara Lisboa <mclisboa2006@gmail.com>
2025-03-18 00:05:55 -0300

    Mudanca

--- app.js
+++ app.js
@@ -7,13 +7,15 @@ function exibirTextoNaTela(tag, texto) {
 
 function adicionarAmigo(){
     let nome = document.querySelector('input').value;
+    
     if(nome.trim() == ''){
         exibirTextoNaTela('h2', 'Por favor insira um nome.');
         return;
     } else {
-    amigos.push(nome);
-    console.log(amigos);
+        amigos.push(nome);
+        console.log(amigos);
     }
+
     exibirTextoNaTela('h2', 'Digite o nome dos seus amigos');
     nome = document.querySelector('input');
     nome.value = '';
