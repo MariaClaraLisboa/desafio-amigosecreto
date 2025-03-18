751deb906c9157d18ca0d5cdb17231f6076169b9
Clara Lisboa <mclisboa2006@gmail.com>
2025-03-18 07:51:59 -0300

    Finalização

--- app.js
+++ app.js
@@ -7,7 +7,7 @@ function exibirTextoNaTela(tag, texto) {
 
 function adicionarAmigo(){
     let nome = document.querySelector('input').value;
-    
+
     if(nome.trim() == ''){
         exibirTextoNaTela('h2', 'Por favor insira um nome.');
         return;
@@ -19,4 +19,29 @@ function adicionarAmigo(){
     exibirTextoNaTela('h2', 'Digite o nome dos seus amigos');
     nome = document.querySelector('input');
     nome.value = '';
+
+    exibirLista();
+}
+
+function exibirLista(){
+    let lista = document.getElementById('listaAmigos'); 
+    lista.innerHTML = "";
+
+    for (let i = 0; i < amigos.length; i++) { 
+        let item = document.createElement('li'); 
+        item.textContent = amigos[i]; 
+        lista.appendChild(item);
+    }
+}
+
+function sortearAmigo(){
+    let indiceAleatorio = parseInt(Math.random()*amigos.length); 
+    console.log(indiceAleatorio);
+
+    let amigo = document.getElementById('resultado');
+    if(amigos.length == 0){
+        amigo.innerHTML = "Não há amigos disponíveis";
+    } else {
+        amigo.innerHTML = `O Amigo sorteado é ${amigos[indiceAleatorio]}`;
+    }
 }
